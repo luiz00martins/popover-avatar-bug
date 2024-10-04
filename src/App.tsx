2fc3fbc5d0b3f30855c5412c89fce093e117fabf
luiz00martins <luizhomartins@gmail.com>
2024-10-04 12:47:33 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import './App.css'
+import "@radix-ui/themes/styles.css";
+import { Avatar, Flex, Popover, Text, Theme } from '@radix-ui/themes'
+
+function App() {
+  return (
+    <Theme>
+      <Flex align="center" justify="center" gap="1" height="100vh">
+        <Flex direction="column" align="center" gap="2">
+          <Text>I work</Text>
+          <Popover.Root>
+            <Popover.Trigger>
+              <Avatar
+                size="6"
+                src="https://randomuser.me/api/portraits/women/10.jpg"
+                fallback="A"
+              />
+            </Popover.Trigger>
+            <Popover.Content>
+              <Text>Popover</Text>
+            </Popover.Content>
+          </Popover.Root>
+        </Flex>
+
+        <Flex direction="column" align="center" gap="2">
+          <Text>I don't</Text>
+          <Popover.Root>
+            <Popover.Trigger>
+              <Avatar
+                size="6"
+                fallback="A"
+              />
+            </Popover.Trigger>
+            <Popover.Content>
+              <Text>Popover</Text>
+            </Popover.Content>
+          </Popover.Root>
+        </Flex>
+      </Flex>
+    </Theme>
+  )
+}
+
+export default App
